960801d49cc707cc1437d406f3e66af019bef6eb
Rohit Bera <rohitnitaibera@gmail.com>
2026-06-18 18:48:41 +0530

    fix updated prisma connection

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,5 +1,8 @@
 import { PrismaClient } from '../generated/prisma/index.js';
 import { PrismaPg } from '@prisma/adapter-pg';
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+  ssl: true,
+});
 export const prisma = new PrismaClient({ adapter });
